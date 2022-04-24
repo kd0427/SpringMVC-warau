90f7e2a9f4e4c6cb0b35f9d69103e3170a80b78c
codud <codud@DESKTOP-AUH74SU>
2022-04-24 18:37:31 +0900

    adopt, petro controller 수정

--- src/main/java/com/global/controller/AdoptController.java
+++ src/main/java/com/global/controller/AdoptController.java
@@ -1,9 +1,16 @@
 package com.global.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.global.service.AdoptService;
+import com.global.vo.AdoptVO;
 
 
 
@@ -11,7 +18,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 public class AdoptController {
 	
+	@Autowired
+	private AdoptService adoptService;
+	
+	
+	@GetMapping("/adopt")
+	public String adopt(Model model) {
 
+		List<AdoptVO> adoptList = adoptService.getList();
+		
+		model.addAttribute("adoptList",adoptList);
+		
+		System.out.println(adoptList);
+		
+		
+		return "board/adopt";
+	}
 
 	@GetMapping("/write")
 	public String write() {
