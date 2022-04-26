ac2381f262111bea6e4e8e8c56da0eaea825eecb
Jiwon <Jiwon@DESKTOP-U6SM3JL>
2022-04-26 18:59:42 +0900

    테스트

---
+++ src/main/java/com/global/controller/InfoController.java
@@ -0,0 +1,46 @@
+package com.global.controller;
+
+
+import java.util.List;
+import com.global.service.InfoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.global.service.AdoptService;
+import com.global.vo.AdoptVO;
+import com.global.vo.InfoVO;
+
+
+
+@Controller
+@RequestMapping("/board")
+public class InfoController {
+	
+	@Autowired
+	private InfoService infoService;
+	
+	
+	@GetMapping("/info")
+	public String info(Model model) {
+
+		List<InfoVO> infoList = infoService.getList();
+		
+		model.addAttribute("infoList",infoList);
+		
+		System.out.println(infoList);
+		
+		
+		return "board/info";
+	}
+
+	@GetMapping("/write")
+	public String write() {
+		
+		
+		return"";
+	}
+}
