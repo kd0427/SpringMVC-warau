b7e589031e4523be3f82fe8d02e3be844ae7134e
codud <codud@DESKTOP-AUH74SU>
2022-04-26 10:04:59 +0900

    test

--- src/main/java/com/global/vo/PetroVO.java
+++ src/main/java/com/global/vo/PetroVO.java
@@ -1,19 +1,28 @@
 package com.global.vo;
 
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public class PetroVO {
 	
 	private int petro_idx;
 	
+	@NotBlank
 	private String petro_title;
+	
+	@NotBlank
 	private String petro_content;
+	
 	private String petro_regdate;
 	
 	private int petro_like;
 	private int petro_hit;
 	
-	private String petro_img;
+	
+	private String petro_img; // 테이블 컬럼 이름
+	private MultipartFile upload_img; // 브라우저가 보낼 파일 데이터 를 멀티파트파일이라는 객체를 담기 위한 변수(클라이언트가 보낼) 
+	
 	private String petro_writer;
 	
 	
@@ -65,6 +74,13 @@ public class PetroVO {
 	public void setPetro_writer(String petro_writer) {
 		this.petro_writer = petro_writer;
 	}
+	public MultipartFile getUpload_img() {
+		return upload_img;
+	}
+	public void setUpload_img(MultipartFile upload_img) {
+		this.upload_img = upload_img;
+	}
+	
 	
 	
 
---
+++ src/main/webapp/WEB-INF/views/board/petro/write.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+    <c:set var="root" value="${pageContext.request.contextPath }/"/>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>Warau</title>
+</head>
+<body>
+
+<!-- top -->
+<c:import url="/WEB-INF/views/include/top_menu.jsp"/>
+
+
+
+
+
+
+<!-- bottom -->
+<c:import url="/WEB-INF/views/include/bottom_info.jsp"/>
+
+</body>
+</html>
